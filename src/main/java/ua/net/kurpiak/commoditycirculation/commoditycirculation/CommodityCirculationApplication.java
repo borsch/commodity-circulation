7ad2d372fd7cde656baf70c7eb56cafaa132e255
Oleh Kurpiak <oleh.kurpiak@gmail.com>
2019-05-05 13:47:55 +0300

    init

---
+++ src/main/java/ua/net/kurpiak/commoditycirculation/commoditycirculation/CommodityCirculationApplication.java
@@ -0,0 +1,13 @@
+package ua.net.kurpiak.commoditycirculation.commoditycirculation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommodityCirculationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommodityCirculationApplication.class, args);
+	}
+
+}
